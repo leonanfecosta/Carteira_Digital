b17fa978e0ab8c13932d77916fb7a8cc5e5af804
Leonan Costa <leonanfecosta@gmail.com>
2022-11-16 09:16:51 -0300

    feat: adiciona arquivos backend

---
+++ backend/src/database/config/database.ts
@@ -0,0 +1,21 @@
+require('dotenv/config');
+
+import { Options } from 'sequelize';
+
+const config: Options = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.PASSWORD || '123456',
+  database: 'NG-app',
+  port: 5432,
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+  define: {
+    timestamps: true,
+  },
+};
+
+module.exports = config;
